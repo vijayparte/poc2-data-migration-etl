666457d0a6ab4f45f843fa11c910d9f2d83472f3
Vijay Parte <vijay.parte@nitorinfotech.com>
2025-03-18 18:02:00 +0530

    Initial commit

---
+++ src/main/java/com/tu/etl/poc2/Poc2DataMigrationEtlApplication.java
@@ -0,0 +1,13 @@
+package com.tu.etl.poc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Poc2DataMigrationEtlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Poc2DataMigrationEtlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tu/etl/poc2/Poc2DataMigrationEtlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tu.etl.poc2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Poc2DataMigrationEtlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
